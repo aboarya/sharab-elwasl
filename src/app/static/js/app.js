ff08bac5323686bd80bf990805019b4344fb5f4d
Sam Mohamed <sam@webiken.net>
2016-07-18 22:27:05 -0400

    site loads after cleanup

--- src/app/static/js/app.js
+++ src/app/static/js/app.js
@@ -15,46 +15,45 @@ var sharabelwasl = angular
     'ngSanitize',
     'pascalprecht.translate',
     'ui.bootstrap'
-  ]);
-//   .constant('DEBUG_MODE', /*DEBUG_MODE*/true/*DEBUG_MODE*/)
-//   .constant('VERSION_TAG', /*VERSION_TAG_START*/new Date().getTime()/*VERSION_TAG_END*/)
-//   .constant('LOCALES', {
-//     'locales': {
-//       'ar': 'العربيه',
-//       'fr': 'Français',
-//       'nl': 'Nederlands',
-//       'de': 'Deutsch',
-//       'en': 'English',
+    // 'tmhDynamicLocale'
+  ])
+  .constant('DEBUG_MODE', /*DEBUG_MODE*/true/*DEBUG_MODE*/)
+  .constant('VERSION_TAG', /*VERSION_TAG_START*/new Date().getTime()/*VERSION_TAG_END*/)
+  .constant('LOCALES', {
+    'locales': {
+      'ar': 'العربيه',
+      'fr': 'Français',
+      'nl': 'Nederlands',
+      'de': 'Deutsch',
+      'en': 'English',
 
       
-//     },
-//     'preferredLocale': 'en'
-//   })
-//   .config(['$interpolateProvider', function($interpolateProvider) {
-//     $interpolateProvider.startSymbol('{a');
-//     $interpolateProvider.endSymbol('a}');
-//   }])
-//   // Angular debug info
-//   .config(function ($compileProvider, DEBUG_MODE) {
-//     if (!DEBUG_MODE) {
-//       $compileProvider.debugInfoEnabled(false);// disables AngularJS debug info
-//     }
-//   })
-//   // Angular Translate
-//   .config(function ($translateProvider, DEBUG_MODE, LOCALES) {
-//     if (DEBUG_MODE) {
-//       $translateProvider.useMissingTranslationHandlerLog();// warns about missing translates
-//     }
+    },
+    'preferredLocale': 'en'
+  })
+  .config(['$interpolateProvider', function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{a');
+    $interpolateProvider.endSymbol('a}');
+  }])
+  // Angular debug info
+  .config(function ($compileProvider, DEBUG_MODE) {
+    if (!DEBUG_MODE) {
+      $compileProvider.debugInfoEnabled(false);// disables AngularJS debug info
+    }
+  })
+  // Angular Translate
+  .config(function ($translateProvider, DEBUG_MODE, LOCALES) {
+    
 
-//     $translateProvider.useStaticFilesLoader({
-//       prefix: 'resources/locale-',
-//       suffix: '.json'
-//     });
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'resources/locale-',
+      suffix: '.json'
+    });
 
-//     $translateProvider.preferredLanguage(LOCALES.preferredLocale);
-//     $translateProvider.useLocalStorage();
-//   })
-//   // Angular Dynamic Locale
+    $translateProvider.preferredLanguage(LOCALES.preferredLocale);
+    $translateProvider.useLocalStorage();
+  })
+  // Angular Dynamic Locale
 //   .config(function (tmhDynamicLocaleProvider) {
 //     tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
 // });
